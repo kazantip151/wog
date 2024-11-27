191a2def3dd9790cbe93f93f51a9d9acf1203372
alexp <alexp@datapro.co.il>
2024-11-27 10:00:51 +0200

    created 2 functions, imported to main

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
-def welcome(player_name):
-    pass
+def welcome():
+    player_name = input("Enter your name: ")
+    print(f'Hi {player_name} and welcome to the World of Games: The Epic Journey')
+
+
+def start_play():
+    print("Choose a game to play:")
+    print("1. Memory Game - a sequence of numbers will appear for 1 second and you have to guess it back")
+    print("2. Guess Game - guess a number and see if you chose like the computer")
+    print("3. Currency Roullete - try and guess the value of a random amount of USD in ILS")
+
+    game = input("Please select a game (1/2/3): ")
+
+    difficulty = input("Please select difficulty (from 1 to 5): ")
 
